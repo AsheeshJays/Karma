534ce696396cb1fee2d09f6250623d0ee6ef337a
arman7985 <97791437+arman7985@users.noreply.github.com>
2022-07-07 10:13:08 +0530

    Update settings.py

--- Karma/settings.py
+++ Karma/settings.py
@@ -130,7 +130,7 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'karmaonlineshop8@gmail.com'
-EMAIL_HOST_PASSWORD = 'ducat@12345'
+EMAIL_HOST_PASSWORD = 'caqnddabhtmfauyq'
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
